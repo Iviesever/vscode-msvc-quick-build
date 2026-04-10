e421d843f6b1626e511d18207aba4c88129b0bc8
Iviesever <3549874980@qq.com>
2026-04-10 13:44:14 +0800

    fix: invoke scripts in-process to fix PS 5.1 encoding issue

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
 ﻿$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-$psExe = if (Get-Command pwsh.exe -ErrorAction SilentlyContinue) { "pwsh.exe" } else { "powershell.exe" }
-Invoke-Expression "function build { & $psExe -NoProfile -File `"$HOME\bin\build.ps1`" @args }"
-Invoke-Expression "function pbuild { & $psExe -NoProfile -File `"$HOME\bin\pbuild.ps1`" @args }"
+function build { & "$HOME\bin\build.ps1" @args }
+function pbuild { & "$HOME\bin\pbuild.ps1" @args }
