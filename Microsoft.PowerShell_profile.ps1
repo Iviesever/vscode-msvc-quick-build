52cf3ff0923e65a4932d6aa7da626ec25a48dad0
Iviesever <3549874980@qq.com>
2026-04-10 13:37:14 +0800

    fix: make profile compatible with Windows PowerShell 5.1 (fallback from pwsh.exe to powershell.exe)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-function build { & pwsh.exe -NoProfile -File "$HOME\bin\build.ps1" @args }
-function pbuild { & pwsh.exe -NoProfile -File "$HOME\bin\pbuild.ps1" @args }
+$psExe = if (Get-Command pwsh.exe -ErrorAction SilentlyContinue) { "pwsh.exe" } else { "powershell.exe" }
+Invoke-Expression "function build { & $psExe -NoProfile -File `"$HOME\bin\build.ps1`" @args }"
+Invoke-Expression "function pbuild { & $psExe -NoProfile -File `"$HOME\bin\pbuild.ps1`" @args }"
