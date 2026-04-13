6eafeaf2e138666480b7a64c2f73e0df2631ac26
Iviesever <3549874980@qq.com>
2026-04-13 13:50:03 +0800

    fix: profile fallback to standard PowerShell if pwsh (PS7) is not available

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,8 @@
 $OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
-function build { & "$HOME\bin\build.ps1" @args }
+function build {
+    if (Get-Command pwsh.exe -ErrorAction SilentlyContinue) {
+        & pwsh.exe -NoProfile -File "$HOME\bin\build.ps1" @args
+    } else {
+        & "$HOME\bin\build.ps1" @args
+    }
+}
